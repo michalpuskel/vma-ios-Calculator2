7b51566c8b31d3a1e496aebe0ba014f7a65c7e47
misko <misko@miskos-imac.local>
2018-11-09 15:04:43 +0100

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by misko on 09/11/2018.
+//  Copyright © 2018 FMFI UK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
